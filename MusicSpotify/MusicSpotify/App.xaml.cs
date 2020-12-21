e732513eabf8a4d7ed7c7384e7e28e59e72f7afa
Filip Merta <filipmerta.fm@gmail.com>
2020-12-21 22:44:19 +0100

    MVVM and removed MainPage

--- MusicSpotify/MusicSpotify/App.xaml.cs
+++ MusicSpotify/MusicSpotify/App.xaml.cs
@@ -10,7 +10,7 @@ namespace MusicSpotify
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            //MainPage = new MainPage();
         }
 
         protected override void OnStart()
